ca8823cafaa8a4da510d1191d42ccbcd6307b24c
shinkwhek <klklpshin@gmail.com>
2017-03-23 10:48:22 +0900

    [RM] eval

--- bnc/env.ml
+++
@@ -1,8 +0,0 @@
-
-
-type env = (Type.n * result) list
-and result =
-  | DBool of bool
-  | DInt of int
-  | DFloat of float
-  | Closure of Type.n * Syntax.t * env
--- bnc/main.ml
+++ bnc/main.ml
@@ -3,16 +3,5 @@
  * author: shinkwhek
  *)
 
-let file f =
-  let inchan = open_in f in
-  let str = Parser.main Lexer.token (Lexing.from_channel inchan) in
-  let result = Evaluation.eval str [] in
-  let result_str = Show_result.showResult result in
-  print_string result_str
-
 let () =
-
-  let argc = Array.length Sys.argv in
-  if argc != 2 then Format.printf "1 input\n"
-  else let fname = Sys.argv.(1) in
-       file fname
+  print_string "Howdy"
